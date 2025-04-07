482ebc5c520c7ae06bd89f42f16bb57a555b975b
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-04-07 13:06:46 -0600

    Create 3_Búsqueda en Profundidad.py

---
+++ Enfoque en Grafos/3_Búsqueda en Profundidad.py
@@ -0,0 +1,14 @@
+import time 
+from collections import defaultdict
+
+    videojuegos = {
+        'The Witcher 3': ['RPG', 'Aventura', 'Mundo Abierto'],
+    'Skyrim': ['RPG', 'Mundo Abierto', 'Fantasía'],
+    'Dark Souls': ['RPG', 'Difícil', 'Fantasía'],
+    'Overwatch': ['FPS', 'Competitivo', 'Multijugador'],
+    'Valorant': ['FPS', 'Competitivo', 'Multijugador'],
+    'Stardew Valley': ['Simulación', 'Relajante'],
+    'Civilization VI': ['Estrategia', 'Turnos', 'Histórico'],
+    'Portal 2': ['Puzzle', 'FPS', 'Ciencia Ficción'],
+    'Hollow Knight': ['Metroidvania', 'Difícil', 'Aventura']
+}
