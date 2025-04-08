3e4e09982feb77a180c44446214a57362aeb9c5b
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-04-08 01:38:02 -0600

    Create 5_Búsqueda en Profundidad Iterativa.py

---
+++ Enfoque en Grafos/5_Búsqueda en Profundidad Iterativa.py
@@ -0,0 +1,12 @@
+import time
+
+mazmorra = {
+    "entrada": ["sala1", "sala2"],
+    "sala1": ["sala3", "sala4"],
+    "sala2": ["sala5"],
+    "sala3": [],
+    "Sala 4": ["Tesoro"],
+    "Sala 5": [],
+    "Tesoro": []
+
+}
