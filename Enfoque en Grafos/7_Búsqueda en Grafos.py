61e5d82ffd48bb4ae3fd81fabf70ebeb77422abd
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-04-17 23:04:15 -0600

    Update 7_Búsqueda en Grafos.py

--- Enfoque en Grafos/7_Búsqueda en Grafos.py
+++ Enfoque en Grafos/7_Búsqueda en Grafos.py
@@ -3,3 +3,12 @@ def recomendar_amigos(grafo_red, usuario_actual, niveles=3):
 
     recomendaciones = {i: set() for i in range(1, niveles+1)}
 
+    cola = deque([(usuario_actual, 0)])
+    while cola:
+        usuario, nivel_actual = cola.popleft()
+        for amigo in grafo_red.get(usuario, []):
+            if amigo not in visitados:
+                visitados.add(amigo)
+                if 1 <= nivel_actual + 1 <= niveles:
+                    recomendaciones[nivel_actual + 1].add(amigo)
+                cola.append((amigo, nivel_actual + 1))
