631b46996daa08c94c54875af697a9ff443b1de8
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-04-21 22:51:59 -0600

    Update 12_Búsqueda de Ascensión de Colinas.py

--- Enfoque en Grafos/12_Búsqueda de Ascensión de Colinas.py
+++ Enfoque en Grafos/12_Búsqueda de Ascensión de Colinas.py
@@ -13,3 +13,15 @@ class RepartidorPizzas:
     def distancia(self, a: Punto, b: Punto) -> float:
         return sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 
+    def distancia_total(self, ruta: List[Punto]) -> float:
+        total = 0
+        for i in range(len(ruta)-1):
+            total += self.distancia(ruta[i], ruta[i+1])
+        total += self.distancia(ruta[-1], ruta[0])
+        return total
+    
+        def generar_vecino(self, ruta: List[Punto]) -> List[Punto]:
+        vecino = ruta.copy()
+        i, j = random.sample(range(len(vecino)), 2)
+        vecino[i], vecino[j] = vecino[j], vecino[i]
+        return vecino
